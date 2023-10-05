64d389faa51247f4cc9069c438d3b3a219902c40
Bruce <98733845+BruceXDong@users.noreply.github.com>
2023-10-04 22:07:56 -0700

    Test commit

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 print("Hello, World!")
-print("Bruce: I'm here!")
-
-
-print("Bruce: I'm here!")
-print("Bruce: I'm here!")
-
-print("Bruce: I'm here!")
-print("Bruce: I'm here!")
-print("Bruce: I'm here!")
 
 print("Bruce: I'm here!!!!!!")
 
